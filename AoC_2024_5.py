f5dc74b42acb92123b60ebf0b11171ed6eb7f579
Nino Dragicevic <nino.dragicevic@probotica.hr>
2024-12-18 16:16:23 +0100

    5_2

--- AoC_2024_5.py
+++ AoC_2024_5.py
@@ -1,8 +1,9 @@
 instructions = []
 updates_l = []
-with open('AoC_2024_5.txt', 'r') as file:
+with open('test.txt', 'r') as file:
     data = file.read().splitlines()
-    updates_str = data[1177:1373]
+    #updates_str = data[1177:1373]
+    updates_str = data[22:28]
     for element in updates_str:
         element = element.split(',')
         updates_l.append(element)
@@ -25,7 +26,20 @@ def check_function(instructions, element, current, slice, length, total, i):
                 return total, bad
     return total, bad
 
+def sort_function(element, instructions):
+    bad = True
+    c = 1
+    while bad or c < (len(element)-1):
+        current = element[c]
+        slice = element[:c]
+        for instance in instructions:
+            if (instance[1] in slice) and instance[0] == current:
+                element[len(slice)], element[len(slice)-1] = element[len(slice)-1], element[len(slice)]
+                c = 1
+        ...
+
 total = 0
+total_2 = 0
 
 for element in updates_l:
     length = len(element)
@@ -34,6 +48,8 @@ for element in updates_l:
         slice = element[:i]
         total, bad = check_function(instructions, element, current, slice, length, total, i)
         if bad:
+            element_sorted = sort_function(element, instructions)
+            total_2 = total + int(element_sorted[(length-1)//2])
             break
 
-print(total)
+print(total, total_2)
