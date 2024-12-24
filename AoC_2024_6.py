97a6f64eab182e746ca1c3dad267c2c00506e448
Nino Dragicevic <nino.dragicevic@probotica.hr>
2024-12-24 09:21:07 +0100

    6

--- AoC_2024_6.py
+++ AoC_2024_6.py
@@ -17,24 +17,37 @@ max = row_length*column_length
 orientations = ['up', 'right', 'down', 'left']
 start_orientation = orientations[i]
 current_orientation = start_orientation
+last_move = 0
 counter = 1
 
-def move(current):
+def move(current, current_orientation, i, last_move):
     if data.find("#", current) == current:
-        return current
+        i = (i + 1) % 4
+        current = current - last_move
+        current_orientation = orientations[i]
     if current_orientation == 'up':
+        last_move = up
         current = current + up
     if current_orientation == 'down':
+        last_move = down
         current = current + down
     if current_orientation == 'left':
+        last_move = left
         current = current + left
     if current_orientation == 'right':
+        last_move = right
         current = current + right
-    return current
+    return current, current_orientation, i, last_move
 
+def mark_trail(data, current):
+    if data[current] != ('#') :
+        data = data[: current] + 'X' + data[current + 1:]
+    return data
 
 while 0 < current_move < max:
-    next_move = move(current_move)
-    if next_move == current_move:
-        current_orientation = orientations[i+1]
+    next_move, current_orientation, i, last_move = move(current_move, current_orientation, i, last_move)
+    data = mark_trail(data, current_move)
+    current_move = next_move
+    if data[current_move] == '\n':
         break
+print(data.count('X'))
